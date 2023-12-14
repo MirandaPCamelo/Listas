b98c50979f402a4c0b8a75cf6d94312e0b54e2ec
MirandaPCamelo <152246653+MirandaPCamelo@users.noreply.github.com>
2023-12-14 08:40:30 -0500

    Update Node.java

--- ListaLigadaSimple/Node.java
+++ ListaLigadaSimple/Node.java
@@ -1,6 +1,12 @@
-//clase autorreferenciada
-
-public class Node {
-	String name; //campo de datos
-    Node next;   //campo enlace
+/**
+ * Creación del nodo y los enlaces.
+ * 
+ * @author (Miranda Patricia Pérez Camelo) 
+ * @version (Noviembre 27, 2023)
+ */
+public class Node
+{
+        //String name;
+        Object name; //la clave del nodo (lo que tiene adentro)
+        Node next; //flechita de enlace para el siguiente nodo
 }
